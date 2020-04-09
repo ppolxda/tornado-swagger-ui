a641e69852cd53d0b8c808f4c20b98a1317ee632
jzd <ppol850564@gmail.com>
2020-04-09 16:36:48 +0800

    add reverse_url

--- tornado_swagger_ui/tornado_swagger_ui.py
+++ tornado_swagger_ui/tornado_swagger_ui.py
@@ -7,12 +7,16 @@ import tornado.web
 
 class SwaggerHandler(tornado.web.RequestHandler):
 
-    def initialize(self, path, base_url, app_name, api_url):
+    def initialize(self, path, base_url, app_name, api_url, reverse_url=''):
+        if reverse_url and (reverse_url[0] != '/' or reverse_url[-1] == '/'):
+            raise TypeError('reverse_url format invaild')
+
         self.index = path + "/index.html"
 
         self.base_url = base_url
         self.api_url = api_url
         self.app_name = app_name
+        self.reverse_url = reverse_url
 
         self.oauth_config = None
 
@@ -22,6 +26,9 @@ class SwaggerHandler(tornado.web.RequestHandler):
             'url': self.api_url,
             'layout': 'StandaloneLayout'
         }
+
+    def make_uri(self, url):
+        return ''.join([self.reverse_url, url])
 
     def set_default_headers(self):
         headers = ["origin", "x-csrf-token", "content-type", "accept",
@@ -39,8 +46,8 @@ class SwaggerHandler(tornado.web.RequestHandler):
             'base_url': self.base_url,
             'app_name': self.app_name,
             'config_json': json.dumps(self.default_config),
-            'oauth_config_json': oauth_config_json
-
+            'oauth_config_json': oauth_config_json,
+            'make_uri': self.make_uri
         }
 
         self.render(self.index, **fields)
@@ -51,7 +58,8 @@ def get_tornado_handler(
     api_url,
     app_name="Swagger UI",
     config=None,
-    oauth_config=None
+    oauth_config=None,
+    reverse_url=''
 ):
 
     module_path = sys.modules.get("tornado_swagger_ui")
@@ -69,7 +77,8 @@ def get_tornado_handler(
                 "path": dist,
                 "base_url": base_url,
                 "app_name": app_name,
-                "api_url": api_url
+                "api_url": api_url,
+                "reverse_url": reverse_url
             }
         )
     ]
